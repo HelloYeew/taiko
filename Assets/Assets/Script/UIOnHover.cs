fd6870405016bb77df40fd1cf524620be8595534
Phawit Pornwattanakul <me@helloyeew.dev>
2021-06-04 01:39:21 +0700

    Add OnMouseHover event

--- Assets/Assets/Script/UIOnHover.cs
+++ Assets/Assets/Script/UIOnHover.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
-public class UIOnHover : MonoBehaviour
+public class UIOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
+    private Vector3 _cachedScale;
+    
     void Start()
     {
-        
+        _cachedScale = transform.localScale;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerEnter(PointerEventData eventData)
     {
-        
+        transform.localScale = new Vector3(1.2f, 1.2f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = _cachedScale;
     }
 }
